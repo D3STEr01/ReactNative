22fb50033760032443b235e4e65982d697b61757
Sandro Guedes <sandroguedes_9@hotmail.com>
2023-11-19 13:04:16 -0300

    oia

--- src/components/modalError.js
+++ src/components/modalError.js
@@ -1,22 +1,43 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import Modal from 'react-native-modal';
+import React from "react";
+import { View, Text, Button } from "react-native";
+import Modal from "react-native-modal";
 
-function ModalError ({isModalVisible, buttonAction}) {
- 
+function ModalError({ isModalVisible, buttonAction }) {
   return (
     <Modal isVisible={isModalVisible}>
-    <View  style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-      <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10, width: '80%', height: '15%' }}>
-        <Text style={{ justifyContent: 'center', alignItems: 'center', marginBottom: 10, textAlign:'center' }}>Email ou senha incorretos</Text>
-        <Button
-        onPress={buttonAction}
-        title={'OK'}
-      />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "white",
+            padding: 20,
+            borderRadius: 10,
+            width: "80%",
+            height: "15%",
+
+          }}
+        >
+          <Text
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              marginBottom: 10,
+              textAlign: "center",
+            }}
+          >
+            Email ou senha incorretos
+          </Text>
+          <Button onPress={buttonAction} title={"OK"} />
+        </View>
       </View>
-    </View>
     </Modal>
   );
-};
+}
 
 export default ModalError;
